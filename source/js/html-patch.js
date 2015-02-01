42eca13e8cbe8571ada90691bcd5fb778893cb27
ppoffice <ppoffice_2008@163.com>
2015-02-01 21:49:10 +0800

    First commit

---
+++ source/js/html-patch.js
@@ -0,0 +1,69 @@
+(function($){  
+  //Highlight current nav item
+  $('#main-nav > a').each(function(){
+  	if($('.page-title-link').length > 0){
+      if($(this).html() == $('.page-title-link').html()){
+        $(this).addClass('current');
+      } else if ($(this).attr('href') == $('.page-title-link').attr('data-url')){
+        $(this).addClass('current');
+      }
+    }
+  });
+  $('.main-nav-list > li > a').each(function(){
+    if($('.page-title-link').length > 0){
+      if($(this).html() == $('.page-title-link').html()){
+        $(this).addClass('current');
+      } else if ($(this).attr('href') == $('.page-title-link').attr('data-url')){
+        $(this).addClass('current');
+      }
+    }
+  });
+  //Fold second-level menu
+  $('.main-nav-list-item').hover(
+    function(){
+      if($(window).width() < 480) return;
+      $(this).children('.main-nav-list-child').slideDown('fast');
+    },
+    function(){
+      if($(window).width() < 480) return;
+      $(this).children('.main-nav-list-child').slideUp('fast');
+    }
+  );
+
+  //Add second-level menu mark
+  $('.main-nav-list-item').each(function(){
+    if($(this).find('.main-nav-list-child').length > 0){
+      $(this).addClass('top-level-menu');
+    }
+  });
+
+  //Set thumbnail height
+  function setThumbnailHeight(){    
+    var width = $('.article-summary .thumbnail').width();
+    var height = 245 * width / 520;
+    $('.article-summary .thumbnail').height(height);
+  }
+  setThumbnailHeight();
+  $(window).on('resize', setThumbnailHeight());
+
+  //Image scroll loading
+  $('.main-body-content img').each(function() {
+    $(this).attr('data-url', $(this).attr('src'));
+    $(this).removeAttr('src');
+    $(this).addClass('scrollLoading');
+    $(this).wrap('<div class="img-wrap"></div>');
+  });
+  function setScrollLoading(){
+    $('.scrollLoading').scrollLoading();
+    $('.scrollLoading').each(function() {
+        if($(this).complete) {
+          $(this).unwrap();
+        } else {
+          $(this).on('load', function(){
+            $(this).unwrap();
+          });
+        }
+    });
+  }
+  setScrollLoading();
+})(jQuery);
