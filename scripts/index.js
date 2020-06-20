039848a0c50ad9db5958bdc5fb04ec6b620e9b46
ppoffice <zrp1994@gmail.com>
2020-06-20 17:38:46 -0400

    chore: upgrade project structure

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+/* global hexo */
+const logger = require('hexo-log')();
+
+/**
+ * Print welcome message
+ */
+logger.info(`================================================
+██╗  ██╗██╗   ██╗███████╗███╗   ███╗ █████╗ ███╗   ██╗
+██║  ██║██║   ██║██╔════╝████╗ ████║██╔══██╗████╗  ██║
+███████║██║   ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+██╔══██║██║   ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+██║  ██║╚██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+======================================================`);
+
+/**
+ * Check if all dependencies are installed
+ */
+require('../include/dependency')(hexo);
+
+/**
+ * Configuration file checking and migration
+ */
+require('../include/config')(hexo);
+
+/**
+ * Register Hexo extensions and remove Hexo filters that could cause OOM
+ */
+require('../include/register')(hexo);
